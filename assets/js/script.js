d7034e55c214c3c122bd29ef2b4811f28beb3e1e
Reymark Magsanay <reymark.magsanay@gmail.com>
2019-06-07 23:06:47 +0800

    commit for added styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,14 @@
-  console.log(window.innerWidth)
+console.log(window.innerWidth)
 if(window.innerWidth >= 1024) {
 
     let scrollpos = window.scrollY
     const header = document.querySelector("nav")
-//   const header_height = header.offsetHeight
     const add_class_on_scroll = () => header.classList.add("fade-in")
     const remove_class_on_scroll = () => header.classList.remove("fade-in")
     console.log(window.screen.width)
     window.addEventListener('scroll', function() { 
         scrollpos = window.scrollY;
-        if (scrollpos >= 570) { add_class_on_scroll() }
+        if (scrollpos >= 10) { add_class_on_scroll() }
         else { remove_class_on_scroll() }
         // console.log(scrollpos)
     })
@@ -17,3 +16,8 @@ if(window.innerWidth >= 1024) {
     const header = document.querySelector("nav")
     header.classList.add("mobile-nav");
 }
+
+// .nav-shrink {
+//     min-height: 60px;
+//     padding: 8px 5rem;
+//   }
